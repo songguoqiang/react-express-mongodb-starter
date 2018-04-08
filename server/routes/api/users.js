c89bcda151497a9339628f82c095e96426da8548
songguoqiang <songguoq@gmail.com>
2018-04-08 11:36:06 +0800

    renamed user_middleware to user_controller

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,38 +1,29 @@
 const router = require("express").Router();
 const handleAsyncError = require("express-async-wrap");
-const userMiddleware = require("../../middlewares/user_middleware");
+const {
+  registerNewUser,
+  login,
+  sendPasswordResetEmail,
+  resetPassword,
+  getCurrentUser,
+  updateCurrentUser,
+  deleteCurrentUser
+} = require("../../controllers/user_controller");
+
 const jwt = require("../../middlewares/jwt_middleware");
 
-router.post("/users/signup", handleAsyncError(userMiddleware.registerNewUser));
+router.post("/users/signup", handleAsyncError(registerNewUser));
 
-router.post("/users/login", userMiddleware.login);
+router.post("/users/login", login);
 
-router.post(
-  "/user/forgot-password",
-  handleAsyncError(userMiddleware.sendPasswordResetEmail)
-);
+router.post("/user/forgot-password", handleAsyncError(sendPasswordResetEmail));
 
-router.post(
-  "/user/reset-password/:token",
-  handleAsyncError(userMiddleware.resetPassword)
-);
+router.post("/user/reset-password/:token", handleAsyncError(resetPassword));
 
-router.get(
-  "/user",
-  jwt.required,
-  handleAsyncError(userMiddleware.getCurrentUser)
-);
+router.get("/user", jwt.required, handleAsyncError(getCurrentUser));
 
-router.put(
-  "/user",
-  jwt.required,
-  handleAsyncError(userMiddleware.updateCurrentUser)
-);
+router.put("/user", jwt.required, handleAsyncError(updateCurrentUser));
 
-router.delete(
-  "/user",
-  jwt.required,
-  handleAsyncError(userMiddleware.deleteCurrentUser)
-);
+router.delete("/user", jwt.required, handleAsyncError(deleteCurrentUser));
 
 module.exports = router;
